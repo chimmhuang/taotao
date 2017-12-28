d097bfa2cbb22b3ec38fbfa7f9dd88584900c96b
chimangmang <chimmhuang@github.com>
2017-12-28 23:10:37 +0800

    update day02

--- taotao-parent/generatorSqlmapCustom/src/GeneratorSqlmap.java
+++ taotao-parent/generatorSqlmapCustom/src/GeneratorSqlmap.java
@@ -18,7 +18,7 @@ public class GeneratorSqlmap {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		File configFile = new File("G:\\IDEA_workspace\\taotao\\taotao-parent\\generatorSqlmapCustom\\generatorConfig.xml");
+		File configFile = new File("generatorConfig.xml"); 
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
